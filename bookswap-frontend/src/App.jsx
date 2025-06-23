519585416ca3b91b075b3549080d0ab836631d43
NavvAbhishek <abi2000navi@gmail.com>
2025-06-23 13:44:11 +0530

    impliment backend auth

---
+++ bookswap-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-red-500 font-bold text-xl">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
